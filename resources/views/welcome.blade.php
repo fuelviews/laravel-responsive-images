dd415426fed502153c438aa98d3b217127966437
skyjebus <joshua@fuelviews.com>
2023-06-18 17:05:42 -0400

    chore: init tests

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-fuelviews::responsive-images
+    :imageName="'logo'"
+    :imageExt="'png'"
+    :imageWidth="'350'"
+    :imageHeight="'167'"
+    :loading="'eager'"
+    :altText="'Alt text'"
+    :class="''"
+/>
