f32012074d011b11d0f4e64e0ae49e154a2f5d24
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2025-11-12 10:21:45 -0500

    Add .gitattributes and update .gitignore to improve version control and ignore unnecessary files; update composer.json to support PHP 8.3 and Laravel 10-12; add Rector configuration for automated code refactoring and Laravel version upgrades. (#17)

---
+++ storage/app/rector/8a/8d/8a8d24c73631e402688d714336fb4b82f8cdf156.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => 'f63f1e732eb0660225262e1f4b9ea4b8',
+));
